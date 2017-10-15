ac55a78a1ba405b1d8167cdfbc153d5689157ef2
Jan <goarano@gmail.com>
2017-10-15 15:05:12 +0200

    added ip as request parameter

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from functools import wraps
 
 from config import AUTH, CONFIG;
 
+
+app = Flask(__name__)
+
+
 def check_auth(hostname, username, password):
     """This function is called to check if a username /
     password combination is valid.
@@ -12,12 +16,14 @@ def check_auth(hostname, username, password):
         return False
     return auth.get('username') == username and auth.get('password') == password
 
+
 def authenticate():
     """Sends a 401 response that enables basic auth"""
     return Response(
     'Could not verify your access level for that URL.\n'
     'You have to login with proper credentials', 401,
     {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 def requires_auth(f):
     @wraps(f)
@@ -28,7 +34,6 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
-app = Flask(__name__)
 
 @app.route("/<hostname>", methods=['GET', 'PUT'])
 @requires_auth
@@ -40,19 +45,26 @@ def endpoint(hostname):
         else:
             return "no saved IP"
     elif request.method == 'PUT':
-        ip = request.remote_addr
-        write_ip(hostname, ip)
+        write_ip(hostname, retrieve_ip())
         return "wrote " + ip
 
+
+# legacy support
 @app.route("/<hostname>/PUT")
+@app.route("/<hostname>/set", methods=['GET', 'POST', 'PUT'])
 @requires_auth
 def endpoint_put(hostname):
-    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    ip = retrieve_ip()
     old_ip = get_ip(hostname)
     write_ip(hostname, ip)
     if old_ip and old_ip != ip:
         return "replaced " + old_ip + " with " + ip
     return "wrote " + ip
+
+
+def retrieve_ip():
+    ip = request.values.get('ip', request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
+
 
 def get_ip(hostname):
     try:
@@ -63,12 +75,15 @@ def get_ip(hostname):
     except FileNotFoundError:
         return None
 
+
 def write_ip(hostname, ip):
     with open(get_file_path(hostname), 'w') as host_file:
         host_file.write(ip)
 
+
 def get_file_path(hostname):
     return CONFIG['NSS_PATH'] + '/' + hostname + '.ddnss'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
