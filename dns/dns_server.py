b860e90a308efc40d3e844f84b70519c6e3d5a64
Jan <goarano@gmail.com>
2019-12-18 14:46:48 +0100

    added mappings

--- dns/dns_server.py
+++ dns/dns_server.py
@@ -1,11 +1,13 @@
 import os
 import ipaddress
 import logging
-from dnslib.dns import DNSRecord, QTYPE, RR
+from dnslib.dns import DNSRecord, DNSLabel, QTYPE, RR
 from dnslib.server import DNSServer, DNSLogger, BaseResolver, DNSHandler
 import requests
 from datetime import datetime
 
+
+log = logging.getLogger(__name__)
 
 DEBUG = os.environ.get('DEBUG')
 SERVER_PORT = int(os.environ.get('SERVER_PORT', '5353'))
@@ -16,13 +18,27 @@ UPSTREAM_ADDRESS = os.environ.get('UPSTREAM_ADDRESS', '1.1.1.1')
 UPSTREAM_PORT = int(os.environ.get('UPSTREAM_PORT', '53'))
 TTL = int(os.environ.get('TTL', '300'))
 USE_TCP = bool(int(os.environ.get('USE_TCP', '0')))
+ZONES = list(map(lambda x: x.strip(), os.environ.get('ZONES', '*.ddnss.').split(',')))
+MAPPINGS = os.environ.get('MAPPINGS', None)
+
+
+def create_mappings(mappings):
+    """ translates `from:to,from2:to2` to dict """
+    ret = dict()
+    if mappings:
+        for m in MAPPINGS.split(','):
+            (fr, to) = tuple(map(lambda x: x.strip(), m.strip().split(':')))
+            dot_ending = lambda x: x.endswith('.') and x or f'{x}.'
+            ret[dot_ending(fr)] = dot_ending(to)
+    return ret
+
 
 KEY_TIME = 'time'
 KEY_VALUE = 'value'
 
 
 class DdnssResolver(BaseResolver):
-    def __init__(self, upstream_address, upstream_port, api_server, api_server_username, api_server_password, zones, ttl):
+    def __init__(self, upstream_address, upstream_port, api_server, api_server_username, api_server_password, zones, ttl, mappings):
         self.upstream_address = upstream_address
         self.upstream_port = upstream_port
         self.api_server = api_server
@@ -31,6 +47,7 @@ class DdnssResolver(BaseResolver):
         self.ttl = ttl
         self.cache = {}
         self.zones = zones
+        self.mappings = mappings
 
     def resolve(self, request: DNSRecord, handler: DNSHandler):
         reply = request.reply()
@@ -65,7 +82,14 @@ class DdnssResolver(BaseResolver):
                 resp_ttl = int(self.ttl - ttl_diff)
         if not ip:
             try:
-                ip = self.ask_api_server(qname, qtype)
+                aname = DNSLabel(str(qname))
+                if self.mappings:
+                    for fr, to in self.mappings.items():
+                        if aname.matchSuffix(fr):
+                            aname = DNSLabel(str(aname).replace(fr, to))
+                            log.debug(f'mapping {qname} to {aname}')
+                            break
+                ip = self.ask_api_server(aname, qtype)
                 ipaddress.ip_address(ip)
                 self.cache[key] = {
                     KEY_TIME: now,
@@ -85,13 +109,16 @@ class DdnssResolver(BaseResolver):
 
 def main():
     if DEBUG:
-        logging.basicConfig(level=logging.DEBUG)
-    logger = DNSLogger(prefix=False)
-    resolver = DdnssResolver(UPSTREAM_ADDRESS, UPSTREAM_PORT, API_SERVER, API_SERVER_USERNAME, API_SERVER_PASSWORD, ["*.ddnss."], TTL)
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-24s %(levelname)-8s %(message)s')
+    logger = DNSLogger()
+    resolver = DdnssResolver(UPSTREAM_ADDRESS, UPSTREAM_PORT, API_SERVER, API_SERVER_USERNAME, API_SERVER_PASSWORD, ZONES, TTL, MAPPINGS)
     server = DNSServer(resolver, port=int(SERVER_PORT), logger=logger, tcp=USE_TCP)
     print(f'running on 0.0.0.0:{SERVER_PORT}/{USE_TCP and "tcp" or "udp"}')
     server.start()
 
 
+MAPPINGS = create_mappings(MAPPINGS)
+ZONES.extend(MAPPINGS.keys())
 if __name__ == '__main__':
+    log = logging.getLogger('dns_server')
     main()
